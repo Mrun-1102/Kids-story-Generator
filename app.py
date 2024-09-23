16540477678dcae4a0c23e2aa668d9a137a33548
Mrunmai Kashyape <mrunmaikashyape2001@gmail.com>
2024-09-23 17:10:29 +0530

    fixed warnings

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning, module="transformers.tokenization_utils_base")
+# import warnings
+# warnings.filterwarnings("ignore", category=FutureWarning, module="transformers.tokenization_utils_base")
 
 
 app = Flask(__name__)
@@ -37,19 +37,23 @@ def generate_story():
     theme = request.form['theme']
 
     # Formulate the prompt for GPT-2
-    prompt = f"Once upon a time in {place}, there was a child named {character_name}. The story is about {theme}. Let's start the adventure!"
+    prompt = f"Once upon a time in {place}, there was a child named {character_name}. The story is about {theme}. Let's start the story!"
     
     # Tokenize the input prompt
     inputs = tokenizer.encode(prompt, return_tensors="pt")
 
     # Generate the story using GPT-2
     outputs = model.generate(inputs, 
-                             max_length=300, 
+                             max_length=100, 
                              num_return_sequences=1, 
+                            #  attention_mask=inputs["attention_mask"],
                              pad_token_id=tokenizer.eos_token_id,
                              temperature=0.7,  # Controls randomness. Lower values make output more focused.
                              top_p=0.9,  # Controls diversity via nucleus sampling.
+                             do_sample=True,
                              repetition_penalty=1.2 ) # Penalize repeating the same word.)
+
+    
 
     # Decode the generated tokens into text
     story = tokenizer.decode(outputs[0], skip_special_tokens=True,clean_up_tokenization_spaces=True)
